d8124d2def27d911e0797a9c028c2e1cd40d0be5
squesalman <sque.salman@gmail.com>
2020-10-30 00:07:19 +0800

    added check_win items

--- tictac.py
+++ tictac.py
@@ -37,7 +37,7 @@ def place_marker(board,marker,position):
 
 #Step 4
 def check_win(board,mark):
-  return board[0] == mark and board[1] == mark and board[2]== mark or board[3] == mark and board[4] == mark and board[5]== mark or board[6] == mark and board[7] == mark and board[8]== mark
+  return board[0] == mark and board[1] == mark and board[2]== mark or board[3] == mark and board[4] == mark and board[5]== mark or board[6] == mark and board[7] == mark and board[8]== mark or board[0] == mark and board[3] == mark and board[6]== mark
 
   
 
